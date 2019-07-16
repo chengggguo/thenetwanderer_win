118e95dfe496d0fe0462cb8ee317f617b5b37507
My Name <my-name@chromium.org>
2019-07-16 13:59:40 +0800

    reset cnc postition done

--- app.js
+++ app.js
@@ -122,6 +122,7 @@ io.on('connection', function(socket){
 	    		/*conver the svg to gcode file and save*/
 				var gcodeData = gcode.gCode;
     			var newStart = 5
+    			var resetPos = 0 - newStart*(PosCounter-1)
     			// console.log('newStart' + newStart)
 	    		var gcodeString = "G1 F1500\nG21\nM05 G4 P0.1 S0\n"; // for servo version
 				// var gcodeString = "G1 F1500\nG21\nM3 S1000\n";
@@ -129,20 +130,21 @@ io.on('connection', function(socket){
     			var gcodeEnding = " "
 
 	    		/*need to check the part belowwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww*/
-    			if (PosCounter != 33){
-    				gcodeEnding = "G0 X" + newStart + " " + "Y0\n"// servo version, in servo version, the pin of M(was spindle on off in stepper version)
+    			if (PosCounter <= 30){
+    				gcodeEnding = "G0 X" + newStart + " " + "Y0\n"// servo version
     				// gcodeEnding = "M3 0\nG0 X" + newStart + " " + "Y0\n"
     				console.log('reach the end')
     			} else{
-    				gcodeEnding = "G0 X0 Y0\n"
-    				//gcodeEnding = "M3 0\nG0 X10 Y0\n" // servo version
+    				gcodeEnding = "G0 X" +resetPos + " " + "Y0\n"
+    				// gcodeEnding = "M3 0\nG0 X10 Y0\n"
+    				PosCounter = 1
     				console.log('far from the end')
     			}
     /**/
 
 
 				for(var i=0; i<gcodeData.length; i++){
-				/*UNCOMMENT TO CHANGE THE VALUE OF Z*///this is comment for stepper version
+				/*UNCOMMENT TO CHANGE THE VALUE OF Z*/
 					var g = gcodeData[i].includes("G1");
 					var n = gcodeData[i].includes("Z");
     				var gStart = gcodeData[i].includes("G9")
@@ -272,11 +274,11 @@ function GcodeSender(callback){
   lr.on('end',function(){
 //    port.write("$H\n\r")
     PosCounter += 1
-    if (PosCounter == 34){
-      portCNC.write("$21=1\n$H\nG0 X-320 Y0\n$21=0\n")
-      console.log('reach end')
-      PosCounter = 0
-    }
+    // if (PosCounter == 34){
+    //   portCNC.write("$21=1\n$H\nG0 X-320 Y0\n$21=0\n")
+    //   console.log('reach end')
+    //   PosCounter = 0
+    // }
     console.log('gcode file all line sent');
     portSpindle.write("2\n")
     callback();
@@ -284,88 +286,89 @@ function GcodeSender(callback){
   return ('finish')
 }
 
-function GcodeMod(callback){
-/*transform text to svg image*/
-  		const svgData = textToSVG.getD(totalData, options);
+// function GcodeMod(callback){
+// /*transform text to svg image*/
+//   		const svgData = textToSVG.getD(totalData, options);
 
-    	/*rotate the svg image and save*/
-  		var transformed = svgpath(svgData).rotate(270, 0, 0).scale(-0.055, 0.055).translate(0,0).toString(); //modify the size and direction of gcode
-  		var path = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"><path fill= \"none\" stroke=\"black\" stroke-width=\"0\" d=\"" + transformed + "\"/></svg>";
-		//console.log(path);
-		fs.writeFile("./test.svg", path, function(){
-  			console.log("SVG Saved");
-  			const gcode = svgcode()
-			.loadFile(__dirname + "/test.svg")
-			.generateGcode();
+//     	/*rotate the svg image and save*/
+//   		var transformed = svgpath(svgData).rotate(270, 0, 0).scale(-0.055, 0.055).translate(0,0).toString(); //modify the size and direction of gcode
+//   		var path = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"><path fill= \"none\" stroke=\"black\" stroke-width=\"0\" d=\"" + transformed + "\"/></svg>";
+// 		//console.log(path);
+// 		fs.writeFile("./test.svg", path, function(){
+//   			console.log("SVG Saved");
+//   			const gcode = svgcode()
+// 			.loadFile(__dirname + "/test.svg")
+// 			.generateGcode();
 
-    		/*conver the svg to gcode file and save*/
-			var gcodeData = gcode.gCode;
-    		var newStart = 5
-    		// console.log('newStart' + newStart)
-    		var gcodeString = "G1 F1500\nG21\nM05 G4 P0.1 S0\n"; // for servo version
-			// var gcodeString = "G1 F1500\nG21\nM3 S1000\n";
-			// var zVal = -1; // comment out on servo version
-    		var gcodeEnding = " "
+//     		/*conver the svg to gcode file and save*/
+// 			var gcodeData = gcode.gCode;
+//     		var newStart = 5
+//     		// console.log('newStart' + newStart)
+//     		var gcodeString = "G1 F1500\nG21\nM05 G4 P0.1 S0\n"; // for servo version
+// 			// var gcodeString = "G1 F1500\nG21\nM3 S1000\n";
+// 			// var zVal = -1; // comment out on servo version
+//     		var gcodeEnding = " "
 
-    		/*need to check the part belowwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww*/
-    		if (PosCounter != 33){
-    			gcodeEnding = "G0 X" + newStart + " " + "Y0\n"// servo version
-    			// gcodeEnding = "M3 0\nG0 X" + newStart + " " + "Y0\n"
-    			console.log('reach the end')
-    		} else{
-    			gcodeEnding = "M3 0\nG0 X10 Y0\n"
-    			console.log('far from the end')
-    		}
-    /**/
+//     		/*need to check the part belowwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww*/
+//     		if (PosCounter <= 2){
+//     			gcodeEnding = "G0 X" + newStart + " " + "Y0\n"// servo version
+//     			// gcodeEnding = "M3 0\nG0 X" + newStart + " " + "Y0\n"
+//     			console.log('reach the eeeend')
+//     		} else{
+//     			gcodeEnding = "M3 0\nG0 X10 Y0\n"
+//     			PosCounter=0
+//     			console.log('far from the end')
+//     		}
+//     /**/
 
 
-			for(var i=0; i<gcodeData.length; i++){
-			/*UNCOMMENT TO CHANGE THE VALUE OF Z*/
-				var g = gcodeData[i].includes("G1");
-				var n = gcodeData[i].includes("Z");
-    			var gStart = gcodeData[i].includes("G9")
-    			var f = gcodeData[i].includes("G0") // new for servo version
-    			var u = gcodeData[i].includes("Z0")
-    			var o = gcodeData[i].includes("X")
-    			var d = gcodeData[i].includes("Z-10")
-				var substring = "";
-				var splitSubs;
-    			if(g && o){
-       				splitSubs = gcodeData[i].split("Z");
-       				//console.log(splitSubs[1]);
-       				gcodeString += splitSubs[0] +"\n";//set F to change the feed speed
-       				//gcodeString += splitSubs[0] + "Z" + zVal + " " + "F150" +"\n";//(spindle versionset F to change the feed speed
-    			}else if(g && d){
-    				splitSubs = gcodeData[i].split("G");
-    				gcodeString += splitSubs[0] + "M03 G4 P0.1 S155" + "\n" // servo down
+// 			for(var i=0; i<gcodeData.length; i++){
+// 			UNCOMMENT TO CHANGE THE VALUE OF Z
+// 				var g = gcodeData[i].includes("G1");
+// 				var n = gcodeData[i].includes("Z");
+//     			var gStart = gcodeData[i].includes("G9")
+//     			var f = gcodeData[i].includes("G0") // new for servo version
+//     			var u = gcodeData[i].includes("Z0")
+//     			var o = gcodeData[i].includes("X")
+//     			var d = gcodeData[i].includes("Z-10")
+// 				var substring = "";
+// 				var splitSubs;
+//     			if(g && o){
+//        				splitSubs = gcodeData[i].split("Z");
+//        				//console.log(splitSubs[1]);
+//        				gcodeString += splitSubs[0] +"\n";//set F to change the feed speed
+//        				//gcodeString += splitSubs[0] + "Z" + zVal + " " + "F150" +"\n";//(spindle versionset F to change the feed speed
+//     			}else if(g && d){
+//     				splitSubs = gcodeData[i].split("G");
+//     				gcodeString += splitSubs[0] + "M03 G4 P0.1 S155" + "\n" // servo down
 
-    			}else if(f && u){
-    				splitSubs = gcodeData[i].split("G");
-    				gcodeString += splitSubs[0] + "M05 G4 P0.1 S0" + "\n" // servo up
+//     			}else if(f && u){
+//     				splitSubs = gcodeData[i].split("G");
+//     				gcodeString += splitSubs[0] + "M05 G4 P0.1 S0" + "\n" // servo up
 
-    			}else if(gStart){
-       				splitSubs = gcodeData[i].split("9")
-       				gcodeString+= splitSubs[0]+ "92 X0 Y0" + "\n" // for serbo version
-       				// gcodeString+= splitSubs[0]+ "92 X0 Y0 Z0" + "\n" //G92 sets current position as new zero
+//     			}else if(gStart){
+//        				splitSubs = gcodeData[i].split("9")
+//        				gcodeString+= splitSubs[0]+ "92 X0 Y0" + "\n" // for serbo version
+//        				// gcodeString+= splitSubs[0]+ "92 X0 Y0 Z0" + "\n" //G92 sets current position as new zero
 
-    			}else{
-    				gcodeString += gcodeData[i]+"\n";
-    			}
-				//gcodeString += gcodeData[i]+"\n"; //COMMENT WHEN CHANGING VALUE OF Z
-			}
-    		gcodeString = gcodeString + gcodeEnding;
+//     			}else{
+//     				gcodeString += gcodeData[i]+"\n";
+//     			}
+// 				//gcodeString += gcodeData[i]+"\n"; //COMMENT WHEN CHANGING VALUE OF Z
+// 			}
+//     		gcodeString = gcodeString + gcodeEnding;
     
-   			console.log('PosCounter'+PosCounter)
+//    			console.log('PosCounter'+PosCounter)
 
 
 
-			//console.log(gcodeString);
-			fs.writeFile("./test.nc", gcodeString, function(){
-  				console.log("GCODE Saved");
-  			});
-  		});
+// 			//console.log(gcodeString);
+// 			fs.writeFile("./test.nc", gcodeString, function(){
+//   				console.log("GCODE Saved");
+//   			});
+//   		});
 
-}
+// }
 
 /*grbl configuration*/
 function ConfigSender(){
